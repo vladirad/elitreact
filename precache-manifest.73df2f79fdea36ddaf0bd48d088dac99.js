dcd302411b1cb9a0eb5ee6b2f32246e0576cc0aa
Vladimir Radisic <vladimir.radisic@outlook.com>
2020-02-07 21:32:02 +0100

    first build

---
+++ precache-manifest.73df2f79fdea36ddaf0bd48d088dac99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "816962633c88fe0f62d896a926135019",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db148ce0e99b249032b3",
+    "url": "/static/css/main.d38d930a.chunk.css"
+  },
+  {
+    "revision": "fa526054e082b25aa5cb",
+    "url": "/static/js/2.6b620a14.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.6b620a14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db148ce0e99b249032b3",
+    "url": "/static/js/main.af704b42.chunk.js"
+  },
+  {
+    "revision": "a97d81cc7de9e2751330",
+    "url": "/static/js/runtime-main.18db8d87.js"
+  }
+]);
